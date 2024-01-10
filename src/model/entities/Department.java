1b2b1fa2945e0d5ddce443deb2d09046f96cb0db
Victor Hugo <victorneivadef8@gmail.com>
2024-01-10 18:17:09 -0300

    Department entity class

---
+++ src/model/entities/Department.java
@@ -0,0 +1,69 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Department implements Serializable{
+	
+	
+	private static final long serialVersionUID = 1L;
+	
+	private int id;
+	private String name;
+	
+	public Department() {
+		
+	}
+	
+	public Department(int id, String name) {
+		this.setId(id);
+		this.setName(name);
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Department other = (Department) obj;
+		return id == other.id;
+	}
+	
+	@Override
+	public String toString() {
+		return "Id -> " + this.getId() +
+				"\nName -> "+ this.getName();
+	}
+	
+	
+	
+	
+	
+
+}
